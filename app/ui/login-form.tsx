f0a6536f05d00a3f7982a91ab46cbbfe288adbdb
Edwin Siby <edwin.siby@letmedoit.in>
2024-06-13 17:05:08 +0530

    Update font import in login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-import { lusitana } from '@/app/ui/font'
+import { lusitana } from '@/app/ui/fonts'
 import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline'
 import { ArrowRightIcon } from '@heroicons/react/20/solid'
 import { Button } from './button'
