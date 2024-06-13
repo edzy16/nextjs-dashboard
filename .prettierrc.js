4cc62194f63f4353708a57530955d675e51fbf49
Edwin Siby <edwin.siby@letmedoit.in>
2024-06-13 16:59:44 +0530

    Fix font import and update layout and navigation components

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 4,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/font'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Acme</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+            </head>
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    )
 }
